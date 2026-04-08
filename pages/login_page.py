d942cda3d29826c52e81c47dc05764bf4b415819
MilJav11 <javorsky.milos@yahoo.sk>
2026-04-08 18:36:12 +0200

    Fix: Explicit package installation and Node 24 update

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,13 +2,11 @@ from .base_page import BasePage
 
 class LoginPage(BasePage):
     def login(self, username, password):
-        # 1. Vyplnenie údajov (toto ti už fungovalo)
         self.page.get_by_role("textbox", name="Prihláste sa pomocou e-mailu").fill(username)
         self.page.get_by_role("button", name="Pokračovať").click()
         self.page.get_by_role("textbox", name="Heslo").fill(password)
         self.page.get_by_role("button", name="Prihlásiť sa").click()
         
-        # 2. POISTKA: Ak nás Seznam hodil na ucet.seznam.cz, klikneme na logo
-        # Používame selektor alt='logo', ktorý si našiel
+    
         if "ucet.seznam.cz" in self.page.url:
             self.page.get_by_alt_text("logo").first.click()
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ from pages.mail_page import MailPage
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        # Otvorí prehliadač a spomalí ho, aby si videl, čo robí
         browser = p.chromium.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
         yield page
